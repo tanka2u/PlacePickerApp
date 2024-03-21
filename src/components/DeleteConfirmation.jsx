afe0f10c3a8120260bfdc49907b04fe6f2c3632b
tanka2u <ptanka123@gmail.com>
2024-03-22 00:48:07 +0100

    handling state management

---
+++ src/components/DeleteConfirmation.jsx
@@ -0,0 +1,20 @@
+export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  console.log("TIMER SET");
+  setTimeout(() => {
+    onConfirm();
+  }, 3000);
+  return (
+    <div id="delete-confirmation">
+      <h2>Are you sure?</h2>
+      <p>Do you really want to remove this place?</p>
+      <div id="confirmation-actions">
+        <button onClick={onCancel} className="button-text">
+          No
+        </button>
+        <button onClick={onConfirm} className="button">
+          Yes
+        </button>
+      </div>
+    </div>
+  );
+}
